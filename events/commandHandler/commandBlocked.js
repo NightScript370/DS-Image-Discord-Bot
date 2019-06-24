8f339225ff1d799a7d0c5a4ee4e222f816518a35
Night Yoshi <nightyoshi370@outlook.com>
2019-06-24 01:12:07 +0000

    Try to see who the bot thinks the owner is

--- events/commandHandler/commandBlocked.js
+++ events/commandHandler/commandBlocked.js
@@ -1,26 +1,27 @@
 const { Listener } = require('discord-akairo');
 
 module.exports = class CommandBlockedListener extends Listener {
-  constructor() {
-    super('commandBlocked', {
-      emitter: 'commandHandler',
-      event: 'commandBlocked',
-      category: 'commandHandler'
-    });
-  }
+    constructor() {
+        super('commandBlocked', {
+            emitter: 'commandHandler',
+            event: 'commandBlocked',
+            category: 'commandHandler'
+        });
+    }
 
-  exec(message, command, reason) {
-    const text = {
+    exec(message, command, reason) {
+        console.log(this.client.ownerID);
+        const text = {
 			owner: () => "we're sorry, but the " + command.id + " command may only be used by the bot owners.",
 			guild: () => "we're sorry, but the " + command.id + " command may only be used in a server."
 		}[reason];
 
 		const tag = message.guild ? message.guild.name : `DM`;
-    console.log(`${message.author.username} (#${message.author.id}) was blocked from using ${command.id} in ${tag} because of ${reason}!`);
+        console.log(`${message.author.username} (#${message.author.id}) was blocked from using ${command.id} in ${tag} because of ${reason}!`);
 
 		if (!text) return;
 		if (message.guild ? message.channel.permissionsFor(this.client.user).has('SEND_MESSAGES') : true) {
 			message.reply(text());
+        }
     }
-  }
-}
+};
