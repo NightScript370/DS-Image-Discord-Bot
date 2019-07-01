415a20e9e9f58b896670f1bfd4c8c03b93fc4795
Samplasion <ercolino2004@gmail.com>
2019-07-01 05:06:02 +0000

    Bug fixes and indentation fix

--- commands/Image Memes/delet.js
+++ commands/Image Memes/delet.js
@@ -3,51 +3,51 @@ const { createCanvas, loadImage } = require('canvas');
 const path = require('path');
 
 module.exports = class DeleteCommand extends Command {
-  constructor() {
-    super('delet', {
-      aliases: ["delet"],
-      category: "Image Memes",
-      description: "Shows an image of the Delete File prompt on Windows.",
-      clientPermissions: ['ATTACH_FILES'],
-      args: [
-	      {
-					id: 'images',
-					type: 'image',
-					match: 'rest'
-				}
-			],
-    });
-  }
+    constructor() {
+        super('delet', {
+            aliases: ["delet"],
+            category: "Image Memes",
+            description: "Shows an image of the Delete File prompt on Windows.",
+            clientPermissions: ['ATTACH_FILES'],
+            args: [
+                {
+                    id: 'images',
+                    type: 'image',
+                    match: 'rest'
+                }
+            ],
+        });
+   }
 
   async exec(message, { images }) {
     let currentimage, widthpad, heightpad;
     try {
-      let deleteimage = await loadImage(path.join(__dirname, '..', '..', 'assets', 'images', 'delete.png'));
+        let deleteimage = await loadImage(path.join(__dirname, '..', '..', 'assets', 'images', 'delete.png'));
 
-			const canvas = createCanvas(deleteimage.width, deleteimage.height);
-			const ctx = canvas.getContext('2d');
+		const canvas = createCanvas(deleteimage.width, deleteimage.height);
+		const ctx = canvas.getContext('2d');
 
-      const imagessize = await this.largestSize(images);
-			const tempcanvas = await createCanvas(imagessize.width, imagessize.height);
-			const tempctx = canvas.getContext('2d');
+        const imagessize = await this.largestSize(images);
+		const tempcanvas = await createCanvas(imagessize.width, imagessize.height);
+		const tempctx = tempcanvas.getContext('2d');
+		
+        for (var image of images) {
+			currentimage = await loadImage(image);
 
-      for (var image of images) {
-				currentimage = await loadImage(image);
+			widthpad = (imagessize.width - currentimage.width) / 2;
+			heightpad = (imagessize.height - currentimage.height) / 2;
 
-				widthpad = (imagessize.width - currentimage.width) / 2;
-				heightpad = (imagessize.height - currentimage.height) / 2;
+			tempctx.drawImage(currentimage, widthpad, heightpad, currentimage.width, currentimage.height);
+	    }
 
-				tempctx.drawImage(currentimage, widthpad, heightpad, currentimage.width, currentimage.height);
-			}
+        ctx.drawImage(deleteimage, 0, 0, deleteimage.width, deleteimage.height)
+        ctx.drawImage(tempcanvas, 120, 135, 195, 195)
 
-      ctx.drawImage(deleteimage, 0, 0, deleteimage.width, deleteimage.height)
-      ctx.drawImage(tempctx, 120, 135, 195, 195)
-
-      const attachment = canvas.toBuffer();
-      if (Buffer.byteLength(attachment) > 8e+6) return message.reply('Resulting image was above 8 MB.');
+        const attachment = canvas.toBuffer();
+        if (Buffer.byteLength(attachment) > 8e+6) return message.reply('Resulting image was above 8 MB.');
 			return message.util.send({ files: [{ attachment, name: 'switch.png' }] });
     } catch (e) {
-    return msg.reply(global.getString(msg.author.lang, "Oh no, an error occurred: `{0}`. Try again later!", e.message));
+        return msg.reply(global.getString(msg.author.lang, "Oh no, an error occurred: `{0}`. Try again later!", e.message));
     }
   }
 }
