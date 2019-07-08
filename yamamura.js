f1e2ad6a0433e5b01b4b57b264b646e56ea54dce
Night Yoshi <nightyoshi370@outlook.com>
2019-07-08 20:05:36 +0000

    Update indentation and somewhat of a cleanup for yamamura.js

--- yamamura.js
+++ yamamura.js
@@ -165,101 +165,96 @@ class MyClient extends AkairoClient {
                 if(inexactUsers.size === 1) {
                     return inexactUsers.first();
                 }
+
+                const exactUsers = inexactUsers.filter(memberFilterExact(term));
+                if(exactUsers.size === 1)  return exactUsers.first();
+
+                if(exactUsers.size > 0) inexactUsers = exactUsers;
+                return inexactUsers.first();
+            }
     
-            const exactUsers = inexactUsers.filter(memberFilterExact(term));
-            if(exactUsers.size === 1)  return exactUsers.first();
-    
-            if(exactUsers.size > 0) inexactUsers = exactUsers;
-            return inexactUsers.first();
-          }
-    
-          function memberFilterExact(search) {
-            return mem => mem.username.toLowerCase() === search ||
-              `${mem.username.toLowerCase()}#${mem.discriminator}` === search;
-          }
-    
-          function memberFilterInexact(search) {
-            return mem => mem.username.toLowerCase().includes(search.toLowerCase()) ||
-              `${mem.username.toLowerCase()}#${mem.discriminator}`.includes(search.toLowerCase());
-          }
+            function memberFilterExact(search) {
+                return mem => mem.username.toLowerCase() === search ||
+                    `${mem.username.toLowerCase()}#${mem.discriminator}` === search;
+            }
+
+            function memberFilterInexact(search) {
+                return mem => mem.username.toLowerCase().includes(search.toLowerCase()) ||
+                    `${mem.username.toLowerCase()}#${mem.discriminator}`.includes(search.toLowerCase());
+            }
 		});
-    this.commandHandler.resolver.addType('image', async (message, argument) => {
-      const fileTypeRe = /\.(jpe?g|png|gif|bmp)$/i;
-      const base64 = /data:image\/(jpe?g|png|gif|bmp);base64,([^\"]*)/;
-      const attachment = message.attachments.first();
+        this.commandHandler.resolver.addType('image', async (message, argument) => {
+            const fileTypeRe = /\.(jpe?g|png|gif|bmp)$/i;
+            const base64 = /data:image\/(jpe?g|png|gif|bmp);base64,([^\"]*)/;
+            const attachment = message.attachments.first();
 
-      function validateAttachment(attachment) {
-        if (!attachment) return false;
-        if (!attachment.height || !attachment.width) return false;
-        if (attachment.size > 8e+6) return false;
-        if (!fileTypeRe.test(attachment.name)) return false;
+            function validateAttachment(attachment) {
+                if (!attachment) return false;
+                if (!attachment.height || !attachment.width) return false;
+                if (attachment.size > 8e+6) return false;
+                if (!fileTypeRe.test(attachment.name)) return false;
 
-        return attachment.url;
-      }
+                return attachment.url;
+            }
 
-      if (attachment && validateAttachment(attachment)) {
-        return [ attachment.url ];
-      } else if (argument && !isEmpty(argument)) {
-        let user = await client.commandHandler.resolver.types.get("user-commando")(message, argument);
+            if (attachment && validateAttachment(attachment)) {
+                return [ attachment.url ];
+            } else if (argument && !isEmpty(argument)) {
+                let user = await client.commandHandler.resolver.types.get("user-commando")(message, argument);
 
-        let splittedarguments = argument.split(' ');
-        let returnargument = [];
+                let splittedarguments = argument.split(' ');
+                let returnargument = [];
 
-        for (var splittedargument of splittedarguments) {
-          if (fileTypeRe.test(splittedargument.split(/[#?]/gmi)[0]))
-            returnargument.push(splittedargument);
-        }
+                for (var splittedargument of splittedarguments) {
+                    if (fileTypeRe.test(splittedargument.split(/[#?]/gmi)[0]))
+                        returnargument.push(splittedargument);
+                }
 
-        if (!isEmpty(returnargument)) {
-          return returnargument;
-        } else if (user && !isEmpty(user.displayAvatarURL({format: 'png'}))) {
-          return [ user.displayAvatarURL({ format: 'png', size: 512 }) ];
-        }
-      } else {
-        // Check previous messages
-        const channel = message.channel;
-        let msgs = await channel.messages.fetch({
-          limit: 100,
-        });
-        let att = undefined;
-        let attachments = List.fromArray(msgs.array()).filter(msg => !!msg.attachments && !!msg.attachments.first()).reverse().forEach(msg => {
-          att = validateAttachment(msg.attachments.first());
-        });
-        if (!att) return null;
-        return [ att ];
-      }
+                if (!isEmpty(returnargument)) {
+                    return returnargument;
+                } else if (user && !isEmpty(user.displayAvatarURL({format: 'png'}))) {
+                    return [ user.displayAvatarURL({ format: 'png', size: 512 }) ];
+                }
+            } else {
+                // Check previous messages
+                const channel = message.channel;
+                let msgs = await channel.messages.fetch({
+                    limit: 100,
+                });
+                let att = undefined;
+                let attachments = List.fromArray(msgs.array()).filter(msg => !!msg.attachments && !!msg.attachments.first()).reverse().forEach(msg => {
+                    att = validateAttachment(msg.attachments.first());
+                });
+                if (!att) return null;
+                return [ att ];
+            }
 
-      return null
+            return null
 		});
-    this.commandHandler.resolver.addType('rps', (message, move) => {
-			let returnvalue = false;
-      if (!move) return null;
+        this.commandHandler.resolver.addType('rps', (message, move) => {
+            if (!move) return null;
 
-      let possible = [
-        '🤚', '✋', '🙌', '📰', '🗞', 'p', 'paper', '🙋', '🖐', // Paper
-        '🖖', '✌', '✂', 'scissors', 's',
-        'rock', 'r', '🤜'
-      ];
+            let possible = [
+                '🤚', '✋', '🙌', '📰', '🗞', 'p', 'paper', '🙋', '🖐', // Paper
+                '🖖', '✌', '✂', 'scissors', 's',
+                'rock', 'r', '🤜'
+            ];
 
-      for (var emoji of possible) {
-			  if (move == emoji) {
-				  returnvalue = true;
-			  }
-      }
+            if (possible.includes(move))
+                return move;
 
-      if (returnvalue) return move;
 			return null;
 		});
-    this.commandHandler.resolver.addType('externalIP', (message, address) => {
+        this.commandHandler.resolver.addType('externalIP', (message, address) => {
 			if (!address) return null;
 
-      if (!/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:0*(?:6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9]))?$/.test(address)) {
-        return null;
-      }
+            if (!/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:0*(?:6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9]))?$/.test(address)) {
+                return null;
+            }
 
-      if (address.split(':')[0] == '127.0.0.1') {
-        return null;
-      }
+            if (address.split(':')[0] == '127.0.0.1') {
+                return null;
+            }
 
 			return address;
 		});
@@ -271,13 +266,13 @@ class MyClient extends AkairoClient {
 		this.listenerHandler = new ListenerHandler(this, {
 			directory: './events/'
 		});
-    this.listenerHandler.setEmitters({
-      process: process,
-      commandHandler: this.commandHandler,
-      inhibitorHandler: this.inhibitorHandler,
-      listenerHandler: this.listenerHandler,
-      dbl: this.dbl
-    });
+        this.listenerHandler.setEmitters({
+            process: process,
+            commandHandler: this.commandHandler,
+            inhibitorHandler: this.inhibitorHandler,
+            listenerHandler: this.listenerHandler,
+            dbl: this.dbl
+        });
 
 		this.commandHandler.useInhibitorHandler(this.inhibitorHandler);
 		this.inhibitorHandler.loadAll();
@@ -287,84 +282,83 @@ class MyClient extends AkairoClient {
 
 		this.commandHandler.loadAll();
 
-    this.moderation = require('./utils/moderation.js');
-    this.config = config;
+        this.moderation = require('./utils/moderation.js');
+        this.config = config;
 
-    this.util.embed = () => {return new BackEmbed();}
-    this.util.pad = (n) => n < 10 ? "0"+n : ""+n;
+        this.util.embed = () => {return new BackEmbed();}
+        this.util.pad = (n) => n < 10 ? "0"+n : ""+n;
 
-    this.audio = {};
-    this.audio.active = new Map();
-    this.audio.play = async (msg, client, data) => {
-        let playing;
+        this.audio = {};
+        this.audio.active = new Map();
+        this.audio.play = async (msg, client, data) => {
+            let playing;
 
-        let relinfo = await Youtube.getInfo(`https://www.youtube.com/watch?v=${data.queue[0].related[0].id}`);
-        let embed = client.util.embed()
-            .setTitle(`<:music:494355292948004874> Now Playing: ${data.queue[0].songTitle}`, data.queue[0].url)
-            .setColor("#FF006E")
-            .addField("Requester", data.queue[0].requester, true)
-            .addField("Duration", data.queue[0].length, true)
-            .addField("Related", `**[${data.queue[0].related[0].title}](${relinfo.video_url})** by ${data.queue[0].related[0].author}`)
-            .setTimestamp(data.queue[0].timerequest)
-            .setThumbnail(data.queue[0].thumbnail)
-            .setServerFooter(msg, true);
+            let relinfo = await Youtube.getInfo(`https://www.youtube.com/watch?v=${data.queue[0].related[0].id}`);
+            let embed = client.util.embed()
+                .setTitle(`<:music:494355292948004874> Now Playing: ${data.queue[0].songTitle}`, data.queue[0].url)
+                .setColor("#FF006E")
+                .addField("Requester", data.queue[0].requester, true)
+                .addField("Duration", data.queue[0].length, true)
+                .addField("Related", `**[${data.queue[0].related[0].title}](${relinfo.video_url})** by ${data.queue[0].related[0].author}`)
+                .setTimestamp(data.queue[0].timerequest)
+                .setThumbnail(data.queue[0].thumbnail)
+                .setServerFooter(msg, true);
 
-        try {
-            let lastChannelMessage = await msg.channel.lastMessage;
-            if (lastChannelMessage.author.id == client.user.id) {
-                playing = await lastChannelMessage.edit({embed: embed});
-            } else {
+            try {
+                let lastChannelMessage = await msg.channel.lastMessage;
+                if (lastChannelMessage.author.id == client.user.id) {
+                    playing = await lastChannelMessage.edit({embed: embed});
+                } else {
+                    playing = await msg.channel.send({embed: embed});
+                }
+            } catch (e) {
                 playing = await msg.channel.send({embed: embed});
             }
-        } catch (e) {
-            playing = await msg.channel.send({embed: embed});
+
+            // YTDL is for downloading while Youtube is for info, since ytdl-discord doesn't have info
+            data.dispatcher = data.connection.play(await YTDL(data.queue[0].url), { type: 'opus', volume: false, passes: 3, highWaterMark: 50 })
+                                .on('error', err => {
+                                    console.error('Error occurred in stream dispatcher:', err);
+                                    playing.edit(`An error occurred while playing the song: ${err}`);
+                                    data.dispatcher.guildID = data.guildID;
+                                    client.audio.finish(msg, client, data.dispatcher);
+                                });
+            data.dispatcher.guildID = data.guildID;
+
+            data.dispatcher.once('end', function() {
+                client.audio.finish(msg, client, this);
+            });
+        };
+        this.audio.finish = async (msg, client, dispatcher) => {
+            let fetched = await client.audio.active.get(dispatcher.guildID);
+            let voicechat = client.guilds.get(dispatcher.guildID).me.voice.channel;
+
+            try {
+                const vcsize = await voicechat.members.filter(val => val.id !== client.user.id).size;
+                if(vcsize == 0) {
+                    client.audio.active.delete(dispatcher.guildID);
+
+                    if (voicechat) return voicechat.leave();
+                    dispatcher.destroy()
+                }
+
+                await fetched.queue.shift();
+                if(fetched.queue.length > 0) {
+                    await client.audio.active.set(dispatcher.guildID, fetched);
+                    client.audio.play(msg, client, fetched);
+                } else {
+                    client.audio.active.delete(dispatcher.guildID);
+
+                    if (voicechat) return voicechat.leave();
+                    dispatcher.destroy()
+                }
+            } catch(error) {
+                console.error(error);
+
+                if (voicechat) return voicechat.leave();
+                dispatcher.destroy()
+            }
         }
-
-        // YTDL is for downloading while Youtube is for info, since ytdl-discord doesn't have info
-        data.dispatcher = data.connection.play(await YTDL(data.queue[0].url), { type: 'opus', volume: false, passes: 3, highWaterMark: 50 })
-                            .on('error', err => {
-                                console.error('Error occurred in stream dispatcher:', err);
-                                playing.edit(`An error occurred while playing the song: ${err}`);
-                                data.dispatcher.guildID = data.guildID;
-                                client.audio.finish(msg, client, data.dispatcher);
-                            });
-        data.dispatcher.guildID = data.guildID;
-
-        data.dispatcher.once('end', function() {
-            client.audio.finish(msg, client, this);
-        });
-    };
-    this.audio.finish = async (msg, client, dispatcher) => {
-	    let fetched = await client.audio.active.get(dispatcher.guildID);
-	    try {
-		    const vcsize = await client.guilds.get(dispatcher.guildID).me.voice.channel.members.filter(val => val.id !== client.user.id).size;
-		    if(vcsize == 0) {
-			    active.delete(dispatcher.guildID);
-
-          var vc = await client.guilds.get(dispatcher.guildID).me.voice.channel;
-          if (vc) return vc.leave();
-          dispatcher.destroy()
-        }
-
-        await fetched.queue.shift();
-        if(fetched.queue.length > 0) {
-          await client.audio.active.set(dispatcher.guildID, fetched);
-          client.audio.play(msg, client, fetched);
-        } else {
-          client.audio.active.delete(dispatcher.guildID);
-
-          var vc = await client.guilds.get(dispatcher.guildID).me.voice.channel;
-          if (vc) vc.leave();
-          dispatcher.destroy()
-        }
-      } catch(error) {
-        console.error(error);
-
-        var vc = await client.guilds.get(dispatcher.guildID).me.voice.channel;
-        if (vc) vc.leave();
-        dispatcher.destroy()
-      }
-    }
 	};
 }
 
@@ -378,8 +372,8 @@ function isEmpty(value) { //Function to check if value is really empty or not
 global.List = List;
 
 setTimeout(function(){
-  module.exports = client;
+    module.exports = client;
 
-  // Actually start the server
-  require('./server.js')
+    // Actually start the server
+    require('./server.js')
 }, 2000);
