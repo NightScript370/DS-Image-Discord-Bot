c50f78350838031f39a70463319307e27c952a6d
Night Yoshi <nightyoshi370@outlook.com>
2019-07-18 01:32:30 +0000

    Try new method of checking if the user has permission to send messages (with embeds)

--- struct/TextChannel.js
+++ struct/TextChannel.js
@@ -1,20 +1,12 @@
-const { Structures } = require("discord.js")
+const { Structures } = require("discord.js");
 
 // This extends Discord's native User class with our own methods and properties
 module.exports = Structures.extend("TextChannel", TextChannel => {
   return class YamamuraTextChannel extends TextChannel {
     constructor(...args) {
-      super(...args)
+      super(...args);
+      this.sendable = !this.guild || (this.guild && this.guild.me.hasPermission('SEND_MESSAGES'));
+      this.embedable = !this.guild || (this.guild && this.guild.me.hasPermission('EMBED_LINKS'));
     }
-
-    get sendable() {
-      if (!this.guild) return true;
-      return this.guild.me.hasPermission('SEND_MESSAGES')
-    }
-
-    get embedable() {
-      if (!this.guild) return true;
-      return this.guild.me.hasPermission('EMBED_LINKS')
-    }
-  }
+  };
 });
