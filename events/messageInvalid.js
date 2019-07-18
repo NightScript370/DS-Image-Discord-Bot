70cc6b346d29dc8e88acac36fdccd81f521e4e88
Samplasion <ercolino2004@gmail.com>
2019-07-18 19:42:47 +0200

    Fix commands not being shown
    Silly me. If I continue before actually assigning a value to the variable, it's obvious that it's gonna continue until it finishes

--- events/messageInvalid.js
+++ events/messageInvalid.js
@@ -42,9 +42,8 @@ module.exports = class messageListener extends Listener {
 
             for (const index in distances) {
                 var analyzedcmd = cmds.filter(cmd => cmd.aliases.includes(distances[index].alias))[0];
-                if (currentcmd) console.log(currentcmd.id, analyzedcmd.id)
-                if (!currentcmd || (currentcmd && currentcmd.id == analyzedcmd.id)) continue;
                 currentcmd = analyzedcmd;
+                if (!currentcmd || (currentcmd && currentcmd.id == analyzedcmd.id)) continue;
                 if (currentcmd.description) {
                     description = currentcmd.description;
                     if (currentcmd.description.content)
