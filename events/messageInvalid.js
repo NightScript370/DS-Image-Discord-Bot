1a94cacde810260f9d514463a5a1b00808fab47e
Samplasion <ercolino2004@gmail.com>
2019-07-18 19:39:46 +0200

    Debug: commands are not being shown

--- events/messageInvalid.js
+++ events/messageInvalid.js
@@ -42,7 +42,8 @@ module.exports = class messageListener extends Listener {
 
             for (const index in distances) {
                 var analyzedcmd = cmds.filter(cmd => cmd.aliases.includes(distances[index].alias))[0];
-                if (!currentcmd || (currentcmd.id == analyzedcmd.id)) continue;
+                if (currentcmd) console.log(currentcmd.id, analyzedcmd.id)
+                if (!currentcmd || (currentcmd && currentcmd.id == analyzedcmd.id)) continue;
                 currentcmd = analyzedcmd;
                 if (currentcmd.description) {
                     description = currentcmd.description;
