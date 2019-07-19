dbd789ae9ea83b282ff2281627d99b91f61e263c
Night Yoshi <nightyoshi370@outlook.com>
2019-07-19 17:55:34 +0000

    No samplasion

--- commands/Bot Utilities/commands.js
+++ commands/Bot Utilities/commands.js
@@ -20,15 +20,6 @@ module.exports = class CommandsCommand extends Command {
 		});
 	}
 
-    async regex(message) {
-        return new RegExp(`^asfg$`);
-        /*if(!message.guild) return;
-        let serverconfig = this.client.db.serverconfig.findOne({ guildID: message.guild.id }) || await this.client.setDefaultSettings(msg);
-
-        if (serverconfig && serverconfig.prefix && serverconfig.prefix.value)
-            return serverconfig.prefix.value;*/
-    }
-
 	async exec(msg, { commandName }) {
         const __ = (k, ...v) => global.getString(msg.author.lang, k, ...v)
 
