4fbdcc7ae5c9b8179c681aa443623e5b98406752
Night Yoshi <nightyoshi370@outlook.com>
2019-07-23 04:04:42 +0000

    Add new properties to eval command

--- commands/Bot Utilities/eval.js
+++ commands/Bot Utilities/eval.js
@@ -7,10 +7,10 @@ module.exports = class EvalCommand extends Command {
 			aliases: ['eval'],
 			category: 'Bot Utilities',
 			description: {
-        content: 'Executes JavaScript code. Only the bot owners may use this command.',
-        usage: '<Javascript Code to Execute>',
-        examples: 'msg.channel.send(\'Hi\');'
-      },
+                content: 'Executes JavaScript code. Only the bot owners may use this command.',
+                usage: '<Javascript Code to Execute>',
+                examples: 'msg.channel.send(\'Hi\');'
+            },
 			ownerOnly: true,
 
 			args: [
@@ -18,38 +18,50 @@ module.exports = class EvalCommand extends Command {
 					id: 'script',
 					prompt: 'What code would you like to evaluate?',
 					type: 'string',
-          match: "content"
+                    match: "rest"
+				},
+				{
+				    id: 'deleteog',
+				    match: 'flag',
+				    flag: '--deleteog'
+				},
+				{
+				    id: 'hideresponce',
+				    match: 'flag',
+				    flag: '--hideresponce'
 				}
 			]
 		});
 	}
 
-	async exec(msg, { script }) {
-    const client = this.client;
-    const channel = msg.channel;
-    const message = msg;
+	async exec(msg, { script, hideresponce, deleteog }) {
+        const client = this.client;
+        const channel = msg.channel;
+        const message = msg;
 
-    try {
-      let result = await eval(script);
-      if (typeof result !== 'string') {
-        result = inspect(result, {
-          depth: 0,
-        })
-      }
+        try {
+            let result = await eval(script);
+            if (typeof result !== 'string') {
+                result = inspect(result, {
+                    depth: 0,
+                });
+            }
 
-      if (result.includes(this.client.token)) result = result.replace(this.client.token, '"If someone tried to make you output the token, you were likely being scammed."')
-      if (result.length > 1990) {
-        console.log(result)
-        return msg.reply('Too long to be printed (content got console logged)')
-      }
+            if (result.includes(this.client.token)) result = result.replace(this.client.token, '"If someone tried to make you output the token, you were likely being scammed."');
+            if (result.length > 1990) {
+                console.log(result);
+                msg.reply('Too long to be printed (content got console logged)');
+            } else if(!hideresponce) {
+                msg.channel.send(result, {code: 'js'});
+            }
 
-      msg.channel.send(result, {code: 'js'})
+            if (deleteog)
+                msg.delete();
+        } catch(err) {
+            console.error(err);
 
-    } catch(err) {
-      console.error(err)
-
-      const error = err.toString().replace(this.client.token, '"If someone tried to make you output the token, you were likely being scammed."')
-      return msg.channel.send(error, {code: 'js'})
+            const error = err.toString().replace(this.client.token, '"If someone tried to make you output the token, you were likely being scammed."');
+            return msg.channel.send(error, {code: 'js'});
+        }
     }
-  }
 };
