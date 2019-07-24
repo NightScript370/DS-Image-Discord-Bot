b4acbbc26aabec5dc1f44fd62a6fba95a07bd191
Samplasion <samplasion@gmail.com>
2019-07-24 21:56:31 +0200

    Update mute.js

--- commands/Moderation/mute.js
+++ commands/Moderation/mute.js
@@ -49,11 +49,11 @@ module.exports = class MuteCommand extends Command {
 		if (author.roles.highest.position <= member.roles.highest.position)
 			return msg.reply("You can't mute someone who has a higher role position than you.");
 
-		if (member.hasPermission("MANAGE_MESSAGES") && !author.hasPermission("ADMINISTRATOR"))
-			return msg.reply("You need to have the `Administrator` permission in order to mute moderators");
-
 		if (member.hasPermission("ADMINISTRATOR"))
 			return msg.reply("Administrators can't be muted. It wouldn't affect them.");
+
+		if (member.hasPermission("MANAGE_MESSAGES") && !author.hasPermission("ADMINISTRATOR"))
+			return msg.reply("You need to have the `Administrator` permission in order to mute moderators");
 		
 		if (member.id == author.id)
 			return msg.reply("You can't mute yourself!");
