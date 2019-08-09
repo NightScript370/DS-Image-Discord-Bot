26e9193d25bb8ec3a77032a58f060cac05d7c853
Night Yoshi <nightyoshi370@outlook.com>
2019-08-09 04:51:57 +0000

    console.warn? What?

--- events/botHandler/warn.js
+++ events/botHandler/warn.js
@@ -1,15 +1,15 @@
 const { Listener } = require('discord-akairo');
 
 module.exports = class warnListener extends Listener {
-  constructor() {
-    super('warn', {
-      emitter: 'client',
-      event: 'warn',
-      category: 'botHandler'
-    });
-  }
+    constructor() {
+        super('warn', {
+            emitter: 'client',
+            event: 'warn',
+            category: 'botHandler'
+        });
+    }
 
-    exec(error) {
-		  console.warn(error);
+    exec(warning) {
+		  console.log(warning);
 	}
-}
+};
