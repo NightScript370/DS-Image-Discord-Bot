d292c52a698d5d86a694c0ab67ca05c0f4716d62
Night Yoshi <nightyoshi370@outlook.com>
2019-08-16 05:33:00 +0000

    Website Clean
    - Kill off old OAuth2 method
    - Add a new module to check if the user is authenticated
    - clean up routes.js, using ES6 arrow functions

--- website/routes.js
+++ website/routes.js
@@ -1,36 +1,26 @@
 const passport = require("passport");
 
-module.exports = function(app, client) {
-  app
-    .get("/", (request, response) => {
-      response.render("index");
-    })
-    .get("/login", passport.authenticate("discord", { failureRedirect: "/" }), 
-        function(req, res) {
-            res.redirect("/profile");
-    })
-    .get("/leaderboard", (request, response) => {
-      response.render("leaderboard", { url: request.originalUrl, id: undefined });
-    })
-    .get("/leaderboard/:guildID", (request, response) => {
-      let id = request.params.guildID;
-      if (!id || !client.guilds.has(id)) return response.status(404).render("pages/404");
-      response.render("leaderboard", { id });
-    })
-    .get("/queue/:guildID", (request, response) => {
-      let id = request.params.guildID;
-      if (!id || !client.guilds.has(id)) return response.status(404).render("pages/404");
-      response.render("queue", { id });
-    })
-    .get("/commands", (request, response) => {
-      response.render("commands");
-    })
-    .get("/support", (request, response) => {
-      response.render("support");
-    })
-    .get('*', function(req, res) {
-      res.redirect("/");
-    });
+module.exports = (app, client) => app
+  .get("/", (request, response) => response.render("index"))
+  .get("/login", passport.authenticate("discord", { failureRedirect: "/" }), (request, response) => response.redirect("/profile"))
+  .get("/logout", async (request, response) => {
+    await req.logout();
+    await res.redirect("/");
+  })
+  .get("/leaderboard", (request, response) => response.render("leaderboard", { url: request.originalUrl, id: undefined }))
+  .get("/leaderboard/:guildID", (request, response) => {
+    let id = request.params.guildID;
 
-  return app;
-}
+    if (!id || !client.guilds.has(id))
+      return response.redirect("/leaderboard");
+
+    response.render("leaderboard", { id });
+  })
+  .get("/queue/:guildID", (request, response) => {
+    let id = request.params.guildID;
+    if (!id || !client.guilds.has(id)) return response.status(404).render("pages/404");
+    response.render("queue", { id });
+  })
+  .get("/commands", (request, response) => response.render("commands"))
+  .get("/support", (request, response) => response.render("support"))
+  .get('*', (request, response) => response.redirect("/"));
