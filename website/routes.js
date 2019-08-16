3479ff294c6211a2cce0555b21eb971c34408cda
Night Yoshi <nightyoshi370@outlook.com>
2019-08-16 06:34:42 +0000

    Fix website

--- website/routes.js
+++ website/routes.js
@@ -1,23 +1,23 @@
 const passport = require("passport");
-let parameters = {
+let parameters = (req) => {
   profile: (req.isAuthenticated() ? "/profile" : "/login")
 }
 
 module.exports = (app, client) => app
-  .get("/", (request, response) => response.render("index", parameters))
-  .get("/login", passport.authenticate("discord", { failureRedirect: "/" }), (request, response) => response.redirect("/profile", parameters))
+  .get("/", (request, response) => response.render("index", parameters(request)))
+  .get("/login", passport.authenticate("discord", { failureRedirect: "/" }), (request, response) => response.redirect("/profile", parameters(request)))
   .get("/logout", async (request, response) => {
     await req.logout();
     await res.redirect("/");
   })
-  .get("/leaderboard", (request, response) => response.render("leaderboard", Object.assign(parameters, { url: request.originalUrl, id: undefined })))
+  .get("/leaderboard", (request, response) => response.render("leaderboard", Object.assign(parameters(request), { url: request.originalUrl, id: undefined })))
   .get("/leaderboard/:guildID", (request, response) => {
     let id = request.params.guildID;
 
     if (!id || !client.guilds.has(id))
       return response.redirect("/leaderboard");
 
-    response.render("leaderboard", Object.assign(parameters, { id }));
+    response.render("leaderboard", Object.assign(parameters(request), { id }));
   })
   .get("/queue/:guildID", (request, response) => {
     let id = request.params.guildID;
@@ -25,8 +25,8 @@ module.exports = (app, client) => app
     if (!id || !client.guilds.has(id))
       return response.status(404).render("pages/404");
 
-    response.render("queue", Object.assign(parameters, { id }));
+    response.render("queue", Object.assign(parameters(request), { id }));
   })
-  .get("/commands", (request, response) => response.render("commands", parameters))
-  .get("/support", (request, response) => response.render("support", parameters))
+  .get("/commands", (request, response) => response.render("commands", parameters(request)))
+  .get("/support", (request, response) => response.render("support", parameters(request)))
   .get('*', (request, response) => response.redirect("/"));
