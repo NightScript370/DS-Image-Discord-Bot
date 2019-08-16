3cdc41afff6b51b1aec3785b8b7884e6e3c9f62f
Night Yoshi <nightyoshi370@outlook.com>
2019-08-16 06:32:19 +0000

    Add a link to sign into Yamamura's website

--- website/routes.js
+++ website/routes.js
@@ -1,26 +1,32 @@
 const passport = require("passport");
+let parameters = {
+  profile: (req.isAuthenticated() ? "/profile" : "/login")
+}
 
 module.exports = (app, client) => app
-  .get("/", (request, response) => response.render("index"))
-  .get("/login", passport.authenticate("discord", { failureRedirect: "/" }), (request, response) => response.redirect("/profile"))
+  .get("/", (request, response) => response.render("index", parameters))
+  .get("/login", passport.authenticate("discord", { failureRedirect: "/" }), (request, response) => response.redirect("/profile", parameters))
   .get("/logout", async (request, response) => {
     await req.logout();
     await res.redirect("/");
   })
-  .get("/leaderboard", (request, response) => response.render("leaderboard", { url: request.originalUrl, id: undefined }))
+  .get("/leaderboard", (request, response) => response.render("leaderboard", Object.assign(parameters, { url: request.originalUrl, id: undefined })))
   .get("/leaderboard/:guildID", (request, response) => {
     let id = request.params.guildID;
 
     if (!id || !client.guilds.has(id))
       return response.redirect("/leaderboard");
 
-    response.render("leaderboard", { id });
+    response.render("leaderboard", Object.assign(parameters, { id }));
   })
   .get("/queue/:guildID", (request, response) => {
     let id = request.params.guildID;
-    if (!id || !client.guilds.has(id)) return response.status(404).render("pages/404");
-    response.render("queue", { id });
+
+    if (!id || !client.guilds.has(id))
+      return response.status(404).render("pages/404");
+
+    response.render("queue", Object.assign(parameters, { id }));
   })
-  .get("/commands", (request, response) => response.render("commands"))
-  .get("/support", (request, response) => response.render("support"))
+  .get("/commands", (request, response) => response.render("commands", parameters))
+  .get("/support", (request, response) => response.render("support", parameters))
   .get('*', (request, response) => response.redirect("/"));
