14f6bdf63c86dfdd71cc79a05739c0547d4d13de
NightYoshi370 <NightYoshi370@outlook.com>
2019-08-21 22:09:21 -0700

    Separate types and audio from the main bot file

---
+++ utils/audio.js
@@ -0,0 +1,97 @@
+module.exports = {
+	active: new Map(),
+	play: async (msg, client, data) => {
+		let playing;
+
+		if (!data.connection) {
+			if (!msg.member || !msg.member.voice) return client.audio.finish(msg, client, data.dispatcher);
+
+			if (!msg.guild.voice || (msg.guild.voice && !msg.guild.voice.connection))
+				data.connection = await msg.member.voice.channel.join();
+			else
+				data.connection = msg.guild.voice.connection;
+		}
+
+		data.dispatcher = data.connection.play(await Youtube(data.queue[0].url), { type: 'opus', volume: false, passes: 3 });
+		const start = Date.now();
+
+		data.dispatcher
+			.on('start', async () => {
+				let relinfo = await Youtube.getInfo(`https://www.youtube.com/watch?v=${data.queue[0].related[0].id}`);
+				let embed = client.util.embed()
+					.setTitle(`<:music:494355292948004874> Now Playing: ${data.queue[0].songTitle}`, data.queue[0].url)
+					.setColor("#FF006E")
+					.addField("Requester", data.queue[0].requester, true)
+					.addField("Duration", data.queue[0].length, true)
+					.addField("Related", `**[${data.queue[0].related[0].title}](${relinfo.video_url})** by ${data.queue[0].related[0].author}`)
+					.setTimestamp(data.queue[0].timerequest)
+					.setThumbnail(data.queue[0].thumbnail)
+					.setServerFooter(msg, true);
+
+				try {
+					let lastChannelMessage = await msg.channel.lastMessage;
+					if (lastChannelMessage.author.id == client.user.id) {
+						playing = await lastChannelMessage.edit({embed: embed});
+					} else {
+						playing = await msg.channel.send({embed: embed});
+					}
+				} catch (e) {
+					playing = await msg.channel.send({embed: embed});
+				}
+
+				console.log(`Starting song with length of ${data.queue[0].secs} seconds.`);
+				setTimeout(() => {
+					console.log('Song should only now be over');
+				}, data.queue[0].secs * 1000 + 10000);
+			})
+			.on('error', async (err) => {
+				console.error('Error occurred in stream dispatcher:', err);
+
+				if (playing)
+					playing.edit(`An error occurred while playing the song: ${err.toString()}`);
+				else
+					playing = await msg.channel.send(`An error occurred while playing the song: ${err.toString()}`);
+
+				data.dispatcher.guildID = data.guildID;
+				client.audio.finish(msg, client, data.dispatcher);
+			})
+			.once('finish', reason => {
+				let seconds = Math.round((Date.now() - start) / 1000);
+				console.log(`\tSong was ${data.queue[0].secs} seconds long, ended after ${seconds} seconds; ${(seconds / data.queue[0].secs * 100).toFixed(1)}% played.\n\tEnd reason: ${reason}`);
+
+				data.dispatcher.guildID = data.guildID;
+				client.audio.finish(msg, client, data.dispatcher);
+			})
+	},
+	finish: async (msg, client, dispatcher) => {
+		let fetched = await client.audio.active.get(dispatcher.guildID);
+		let voicechat = client.guilds.get(dispatcher.guildID).me.voice.channel;
+
+		try {
+			const vcsize = await voicechat.members.filter(val => val.id !== client.user.id).size;
+			if (!vcsize) {
+				client.audio.active.delete(dispatcher.guildID);
+
+				if (voicechat)
+					return voicechat.leave();
+				dispatcher.destroy()
+			}
+
+			await fetched.queue.shift();
+			if (fetched.queue.length > 0) {
+				await client.audio.active.set(dispatcher.guildID, fetched);
+				client.audio.play(msg, client, fetched);
+			} else {
+				client.audio.active.delete(dispatcher.guildID);
+
+				if (voicechat) return voicechat.leave();
+				dispatcher.destroy()
+			}
+		} catch(error) {
+			console.error(error);
+
+			if (voicechat) return voicechat.leave();
+			dispatcher.destroy()
+		}
+	}
+}
---
+++ utils/types.js
@@ -0,0 +1,207 @@
+const akairo_types = {
+	'question': (message, phrase) => {
+		if (!phrase || (phrase && !phrase.endsWith('?')) return null;
+		return phrase;
+	},
+	'text-fun': async (message, phrase) => {
+		if (phrase) return phrase;
+
+		let msgs = await message.channel.messages.fetch({ limit: 100 });
+		let msgArr = msgs.array().sort((a, b) => a.createdAt - b.createdAt);
+
+		let target = null;
+		// Get the latest one that has text
+		for (let i = 99; i >= 0; i--) {
+			let msg = msgArr[i];
+			if (message == msg) continue;
+			if (msg && isGood(msg.cleanContent)) {
+				target = msg;
+				break;
+			}
+		}
+
+		if (target)
+			return target.cleanContent;
+
+		return null;
+	},
+	'user-commando': async (message, user) => {
+		if (!user) return null;
+
+		const matches = user.match(/^(?:<@!?)?(\d{17,19})>?$/);
+		if (matches) {
+			let fetchedUser = await message.client.users.fetch(matches[1])
+				.catch();
+
+			if (fetchedUser)
+				return fetchedUser;
+		}
+
+		if (userSearch(message, user.toLowerCase()))
+			return userSearch(message, user.toLowerCase());
+
+		let userFound = null;
+		let tmp_user = user.split(" ")
+		while (!userFound && tmp_user.length > 1) {
+			tmp_user.pop()
+			userFound = userSearch(message, tmp_user.join(" ").toLowerCase());
+		}
+
+		return userFound;
+	},
+	'externalIP': (message, address) => {
+		if (!address)
+			return null;
+
+		if (!/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:0*(?:6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9]))?$/.test(address)) {
+			return null;
+
+		if (address.split(':')[0] == '127.0.0.1') {
+			return null;
+
+		return address;
+	},
+	'image': await imageType(message, argument, true, true),
+	'image-nohistory': await imageType(message, argument, true, false),
+	'image-noattachment': await imageType(message, argument, false, true),
+	'image-nohistoryattachment': await imageType(message, argument, false, true)
+}
+
+// *******************************
+// ********* USER SEARCH *********
+// *******************************
+
+const userSearch = (message, term) => {
+	if (message.guild) {
+		let members = message.guild.members.filter(userFilter(false, term, true));
+		if (members.size) {
+			const exactMembers = members.filter(userFilter(true, term, true));
+
+			if (exactMembers.size > 0)
+				members = exactMembers;
+
+			let guildMember = members.first();
+			return guildMember.user;
+		}
+	}
+
+	let users = message.client.users.filter(userFilterInexact(term));
+	if (!users.size)
+		return null;
+
+	const exactUsers = users.filter(userFilterExact(term));
+
+	if (exactUsers.size > 0)
+		users = exactUsers;
+
+	return users.first();
+}
+
+const userFilter = (exact, search, guild) => {
+	if (guild) {
+		if (exact)
+			return member => member.displayName.toLowerCase() === search || member.user.username.toLowerCase() === search || member.user.tag.toLowerCase() === search;
+		else
+			return member => member.displayName.toLowerCase().includes(search) || member.user.username.toLowerCase().includes(search) || member.user.tag.toLowerCase().includes(search);
+	} else {
+		if (exact)
+			return user => user.username.toLowerCase() === search || user.tag.toLowerCase() === search;
+		else
+			return user => user.username.toLowerCase().includes(search) || user.tag.toLowerCase().includes(search);
+	}
+}
+
+// ***************
+// **** IMAGE ****
+// ***************
+
+const validateAttachment = (attachment) {
+	if (!attachment) return false;
+	if (!attachment.height || !attachment.width) return false;
+	if (attachment.size > 8e+6) return false;
+	if (!fileTypeRe.test(attachment.name)) return false;
+
+	return attachment.url;
+}
+
+function githubRaw(url) {
+	var [https, n, domain, owner, project, blob, branch, ...position] = url.split("/");
+	return `https://raw.githubusercontent.com/${owner}/${project}/${branch}/${position.join("/")}`;
+}
+
+const imageType = async (message, argument, attachmentcheck, historycheck) => {
+	const fileTypeRe = /\.(jpe?g|png|gif|bmp)$/i;
+	const base64 = /data:image\/(jpe?g|png|gif|bmp);base64,([^\"]*)/;
+	const attachment = message.attachments.first();
+
+	if (message.attachments.size && attachmentcheck) {
+		let attachmentListing = Array.from(message.attachments.values);
+		let cleanAttachments;
+
+		for (var attachment of attachmentListing) {
+			if (validateAttachment(attachment))
+				cleanAttachments.push(attachment.url);
+		}
+
+		return cleanAttachments;
+	}
+
+	if (isGood(argument)) {
+		let user = await akairo_types['user-commando'](message, argument);
+
+		let splitArgs = argument.split(' ');
+		let testedArgs = [];
+
+		for (var splitArg of splitArgs) {
+			if (!fileTypeRe.test(splitArg.split(/[#?]/gmi)[0]))
+				continue;
+
+			if (splitArg.startsWith('https://github.com')
+			 || splitArg.startsWith('https://www.github.com')
+			 || splitArg.startsWith('http://github.com')
+			 || splitArg.startsWith('http://www.github.com')) {
+
+				// Convert GitHub URL to raw type
+				splitArg
+					.replace('http://www.github.com', 'https://github.com')
+					.replace('https://www.github.com', 'https://github.com')
+
+				testedArgs.push(githubRaw(splitArg))
+				break;
+			}
+
+			testedArgs.push(splitArg);
+		}
+
+		if (isGood(testedArgs)) {
+			return testedArgs;
+		} else if (user && isGood(user.displayAvatarURL({format: 'png'}))) {
+			return [ user.displayAvatarURL({ format: 'png', size: 512 }) ];
+		}
+	}
+
+	if (historycheck) {
+		// Check previous messages
+		const channel = message.channel;
+		let msgs = await channel.messages.fetch({ limit: 100 });
+		let att = undefined;
+		let attachments = List.fromArray(msgs.array()).filter(msg => !!msg.attachments && !!msg.attachments.first()).reverse().forEach(msg => {
+			att = validateAttachment(msg.attachments.first());
+		});
+		if (!att) return null;
+		return [ att ];
+	}
+
+	return null
+}
+
+
+
+module.exports = akairo_types;
+
+
+
+const isGood(variable) {
+	if (variable && variable !== null && (variable.size || variable.length)) return true;
+		return false;
+}
