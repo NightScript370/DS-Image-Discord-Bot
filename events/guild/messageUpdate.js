5cd6f498f4a72e1e4f606cebe35ad587165ae71a
NightYoshi370 <NightYoshi370@outlook.com>
2019-08-22 16:36:11 -0700

    Fix up messageUpdate

--- events/guild/messageUpdate.js
+++ events/guild/messageUpdate.js
@@ -1,53 +1,54 @@
 const { Listener } = require('discord-akairo');
 
 module.exports = class messageUpdateListener extends Listener {
-  constructor() {
-    super('messageUpdate', {
-      emitter: 'client',
-      event: 'messageUpdate',
-      category: 'guild'
-    });
-  }
+	constructor() {
+		super('messageUpdate', {
+			emitter: 'client',
+			event: 'messageUpdate',
+			category: 'guild'
+		});
+	}
 
-  exec(oldMessage, newMessage) {
-    if (!oldMessage) return;
-    if (!oldMessage.guild) return;
-		if (oldMessage.author.bot) return;
+	exec(oldMessage, newMessage) {
+		if (!oldMessage) return;
+		if (!newMessage.guild) return;
+		if (newMessage.author.bot) return;
 
-    let logs;
+		let logs;
 
-    try {
-      logs = this.client.db.serverconfig.get(this.client, newMessage, "logchan")
-    } catch(e) {
-      console.error(e)
-    }
+		try {
+			logs = this.client.db.serverconfig.get(this.client, newMessage, "logchan")
+		} catch(e) {
+			console.error(e)
+		}
 
-    if (logs && logs.sendable && logs.embedable) {
-  	  let messageUpdateEmbed = this.client.util.embed()
-  		  .setColor("#0000FF")
-	  	  .setThumbnail(newMessage.guild.iconURL({format: 'png'}))
-	  	  .setTimestamp(new Date())
-	  	  .setFooter(`${newMessage.author.tag} (#${newMessage.author.id})`, newMessage.author.displayAvatarURL({format: 'png'}));
+		if (!logs) return;
+		if (!logs.sendable || !logs.embedable) return;
 
-      let text;
-      if (oldMessage.content !== newMessage.content) {
-        text = `${newMessage.member.displayName} updated their message`;
+		let messageUpdateEmbed = this.client.util.embed()
+			.setColor("#0000FF")
+			.setThumbnail(newMessage.guild.iconURL({format: 'png'}))
+			.setTimestamp(new Date())
+			.setFooter(`${newMessage.author.tag} (#${newMessage.author.id})`, newMessage.author.displayAvatarURL({format: 'png'}));
 
-        if (oldMessage.content.length < 1020 && newMessage.content.length < 1020)
-          messageUpdateEmbed
-            .addField("Before", `${oldMessage.content}`, true)
-			      .addField("After", `${newMessage.content}`, true)
-      } else if ((!oldMessage.pinned && newMessage.pinned) && (oldMessage.pinned && !newMessage.pinned)) {
-        text = `A message by ${newMessage.member.displayName} was pinned`;
-        messageUpdateEmbed.setDescription(newMessage.content)
-      } else
-        return;
+		let text;
+		if (oldMessage.content !== newMessage.content) {
+			text = `${newMessage.member.displayName} updated their message`;
 
-      messageUpdateEmbed
-        .addField(":bookmark_tabs: Channel", `${newMessage.channel.name} (#${newMessage.channel.id})`)
+			if (oldMessage.content.length < 1020 && newMessage.content.length < 1020)
+				messageUpdateEmbed
+				.addField("Before", `${oldMessage.content}`, true)
+				.addField("After", `${newMessage.content}`, true)
+		} else if ((!oldMessage.pinned && newMessage.pinned) || (oldMessage.pinned && !newMessage.pinned)) {
+			text = `A message by ${newMessage.member.displayName} was pinned`;
+			messageUpdateEmbed.setDescription(newMessage.content)
+		} else
+			return;
+
+		messageUpdateEmbed
+			.addField(":bookmark_tabs: Channel", `${newMessage.channel.name} (#${newMessage.channel.id})`)
 	  		.addField(":id: Message ID", `${newMessage.id}`)
 
-		  logs.send(text, { embed: messageUpdateEmbed });
-    }
-  }
+		logs.send(text, { embed: messageUpdateEmbed });
+	}
 }
