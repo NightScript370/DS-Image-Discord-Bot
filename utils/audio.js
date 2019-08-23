1cba3716af6b5d8dad7888e152f9b6080c6fe4dc
Night Yoshi <nightyoshi370@outlook.com>
2019-08-23 23:09:54 +0000

    Update audio.js

--- utils/audio.js
+++ utils/audio.js
@@ -14,7 +14,7 @@ module.exports = {
 				data.connection = msg.guild.voice.connection;
 		}
 
-		data.dispatcher = data.connection.play(await Youtube.play(data.queue[0].url, {filter: 'audioonly', quality: 'highestaudio', highWaterMark: 1<<25}), { type: 'opus', volume: false, highWaterMark: 1 });
+		data.dispatcher = data.connection.play(await Youtube.play(data.queue[0].url, {filter: 'audioonly'}), { type: 'opus', volume: false });
 		const start = Date.now();
 
 		data.dispatcher
