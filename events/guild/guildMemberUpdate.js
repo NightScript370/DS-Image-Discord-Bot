a7c47e04cee7fbd0e137fe68c2e1fc0a6b2b931c
Night Yoshi <nightyoshi370@outlook.com>
2019-08-27 23:51:46 +0000

    Fix guildMemberUpdate

--- events/guild/guildMemberUpdate.js
+++ events/guild/guildMemberUpdate.js
@@ -11,7 +11,7 @@ module.exports = class guildMemberUpdateListener extends Listener {
 
 	async exec(oldMember, newMember) {
 		if (oldMember.partial) return;
-		if (newMember.partial) await newMember.fetch();
+		if (newMember.partial) newMember = await newMember.fetch();
 
 		let embed = this.client.util.embed()
 			.setColor('#0000FF')
@@ -42,21 +42,44 @@ module.exports = class guildMemberUpdateListener extends Listener {
 			}
 		}
 
-		if (newMember.roles.length !== oldMember.roles.length || !isEmpty(newMember.roles.filter(r => !oldMember.roles.has(r)))) {
-			const added = newMember.guild.roles.get(newMember.roles.find(r => !oldMember.roles.has(r)));
-			const removed = newMember.guild.roles.get(oldMember.roles.find(r => !newMember.roles.has(r)));
+		if (newMember.roles.size !== oldMember.roles.size) {
+			let changedRoles = [];
 
-			if (added) embed.addField('Added Role', added)
-			if (removed) embed.addField('Removed Role', removed)
+        	if (newMember.roles.size < oldMember.roles.size) {
+				for (var role of oldMember.roles) {
+					if (!newMember.roles.has(role.id)) continue;
+					changedRoles.push(`${role.name} (#${role.id})`);
+				}
 
-			let finalroles = newMember.roles.filter(r => r.id != newMember.guild.id).map(r => r.name).join(", ");
-			if (!isEmpty(finalroles))	embed.addField('Final Role Listing', '```'+finalroles+'```')
-			else											embed.addField('Final Role Listing', 'None')
+				embed.addInline('Removed Roles', changedRoles.join("\n"));
+        	} else {
+				for (var role of newMember.roles) {
+					if (!oldMember.roles.has(role.id)) continue;
+					changedRoles.push(`${role.name} (#${role.id})`);
+				}
+
+				embed.addInline('Added Roles', changedRoles.join("\n"))
+        	}
+
+			const roles = (newMember.roles ? newMember.roles
+				.filter(role => role.id !== newMember.guild.roles.everyone.id)
+			    .sort((a, b) => b.position - a.position)
+			    .map(role => role.name) : []);
+
+			embed.addField(`Final Role Listing (${roles.length})`, roles.length ? this.trimArray(roles).join(', ') : 'None')
 		}
 
 		let logchannel = this.client.db.serverconfig.get(this.client, newMember, "logchan")
 		if (logchannel && logchannel.sendable && logchannel.embedable)
 			logchannel.send({embed});
+	}
+
+	trimArray(arr) {
+		if (arr.length > 10) {
+			const len = arr.length - 10;
+			arr = arr.slice(0, 10);
+		}
+		return arr;
 	}
 } 
 
