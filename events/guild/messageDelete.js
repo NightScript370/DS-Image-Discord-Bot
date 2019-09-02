a5cf6d1188431de3d62d0121769ae97c924024bf
NightYoshi370 <maorninja@outlook.com>
2019-09-01 22:36:16 -0700

    add image linking for messageDelete

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -14,6 +14,7 @@ module.exports = class MessageDeleteListener extends Listener {
 
 		if (!message.guild) return;
 		if (message.partial) return;
+		if (message.author.partial) await message.author.fetch();
 		if (message.author.id == this.client.user.id && message.content.endsWith("messages deleted!")) return;
 		if (message.author.id == this.client.user.id && message.content.endsWith("you may not star bot messages.")) return;
 		if (message.author.id == this.client.user.id && message.content.endsWith("you may not star your own message.")) return;
@@ -21,30 +22,41 @@ module.exports = class MessageDeleteListener extends Listener {
 		const logChannel = this.client.db.serverconfig.get(this.client, message, "logchan");
 
 		if (logChannel && logChannel.sendable && logChannel.embedable) {
+			let title = `:scissors: Message from ${message.author.username} deleted`;
 			let messageDeleteEmbed = this.client.util.embed()
 				.setColor("#ff0000")
 				.setThumbnail(message.guild.iconURL({format: 'png'}))
-				.setDescription(message.content)
-				.addField(":bookmark_tabs: Channel", `${message.channel.name} (#${message.channel.id})`)
-				.addField(":id: Message ID", message.id)
-				.setFooter(`${message.author.tag} (#${message.author.id})`, message.author.displayAvatarURL({format: 'png'}))
-				.setTimestamp(new Date());
+				.setDescription(message.content);
 
-			const image = message.attachments.size > 0 ? extension(message.attachments.array()[0].url) : '';
-			if(!isEmpty(image))	messageDeleteEmbed.setImage(image);
-
-			let title = `:scissors: Message from ${message.author.username} deleted`;
 			if (message.guild.me.hasPermission('VIEW_AUDIT_LOG')) {
 				try {
-					const entry = message.guild.fetchAuditLogs({type: 'MESSAGE_DELETE'}).then(audit => audit.entries.first());
+					const entry = await message.guild.fetchAuditLogs({type: 'MESSAGE_DELETE', limit: 1}).then(audit => audit.entries.first());
+					if (entry) {
+						const executor = entry.executor;
+						if (!executor || executor.partial) await executor.fetch();
 
-					if (entry.extra && entry.extra.channel.id === message.channel.id
-					&& (entry.target.id === message.author.id)
-					&& (entry.createdTimestamp > (Date.now() - 5000))
-					&& (entry.extra.count >= 1))
-						title += ` by ${entry.executor.username}`;
+						if (entry.extra && entry.extra.channel.id === message.channel.id
+						&& (entry.target.id === message.author.id)
+						&& (entry.createdTimestamp > (Date.now() - 5000))
+						&& (entry.extra.count >= 1))
+							messageDeleteEmbed.addField(':cop: Moderator', executor.username + (entry.reason ? ` - ${entry.reason}` : ''))
+					}
 				} catch {}
 			}
+
+			messageDeleteEmbed
+				.addInline(":bookmark_tabs: Channel", `${message.channel.name} (#${message.channel.id})`)
+				.addInline(":id: Message ID", message.id);
+
+			const attachment = message.attachments.size > 0 && extention(message.attachments.first()) ? message.attachments.first().proxyURL : '';
+			if(!isEmpty(attachment))
+				messageDeleteEmbed
+					.attachFiles([attachment])
+					.setImage(fileName(attachment));
+
+			messageDeleteEmbed
+				.setFooter(`${message.author.tag} (#${message.author.id})`, message.author.displayAvatarURL({format: 'png'}))
+				.setTimestamp(new Date());
 
 			logChannel.send(title, {embed: messageDeleteEmbed});
 		}
@@ -57,6 +69,11 @@ function extension(attachment) {
 	const image = /(jpg|jpeg|png|gif)/gi.test(typeOfImage);
 	if (!image) return '';
 	return attachment;
+}
+
+function fileName(attachment) {
+	const split = attachment.split('/');
+	return split[split.length - 1];
 }
 
 function isEmpty(value) {
