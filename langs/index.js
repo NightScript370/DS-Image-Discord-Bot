a305a9a6fcf501e96724d22f8c851b9b15b5d8a5
Epicpkmn11 <epicpkmn11@outlook.com>
2019-09-07 18:16:49 -0500

    Add Japanese to language list

--- langs/index.js
+++ langs/index.js
@@ -18,6 +18,12 @@ module.exports = [
     translators: ["Samplasion#0325", "Miky88#3229"]
   },
   {
+    code: "jp",
+    name: "日本語 (Japanese)",
+    flag: ":flag_jp:",
+    translators: ["Pk11#1288"]
+  },
+  {
     code: "pl",
     name: "Polski (Polish)",
     flag: ":flag_pl:",
