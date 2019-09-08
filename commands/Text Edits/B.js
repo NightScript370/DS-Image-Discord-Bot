325b7d8a184878df0f4fdf14072ddcc2cdf9cac0
NightYoshi370 <maorninja@outlook.com>
2019-09-07 23:52:19 -0700

    Fix B in normal channel

--- commands/Text Edits/B.js
+++ commands/Text Edits/B.js
@@ -1,28 +1,28 @@
 const { Command } = require('discord-akairo');
 
 module.exports = class OwoCommand extends Command {
-  constructor() {
-    super('b', {
-      category: 'Text Edits',
-      aliases: ["b", '🅱'],
-      description: {
-        content: 'replace every b/B with a 🅱.',
-        usage: '<text you\'d like to transform to a 🅱>'
-      },
-      args: [{
-        id: 'toB',
-        type: 'text-fun',
-        match: 'content'
-      }]
-    });
-  }
+	constructor() {
+		super('b', {
+			category: 'Text Edits',
+			aliases: ["b", '🅱'],
+			description: {
+				content: 'replace every b/B with a 🅱.',
+				usage: '<text you\'d like to transform to a 🅱>'
+			},
+			args: [{
+				id: 'toB',
+				type: 'text-fun',
+				match: 'content'
+			}]
+		});
+	}
 
-  exec(message, { toB }) {
-    if (!toB.includes('b' || 'B'))
-      return message.util.send(global.getString(message.author.lang, 'There was no Bs found in the text'));
+	exec(message, { toB }) {
+		if (!toB || (toB && (!toB.includes('b') || !toB.includes('B'))))
+			return message.util.send(global.getString(message.author.lang, 'There was no Bs found in the text'));
 
-    let author = message.guild ? message.member.displayName : message.author.username;
-    let text = toB.replace(/b/gi, "🅱").replace(/B/gi, "🅱")
-    message.util.send(author + ` says: ` + text);
-  }
+		let author = message.guild ? message.member.displayName : message.author.username;
+		let text = toB.replace(/b/gi, "🅱").replace(/B/gi, "🅱")
+		message.util.send(author + ` says: ` + text);
+	}
 };
