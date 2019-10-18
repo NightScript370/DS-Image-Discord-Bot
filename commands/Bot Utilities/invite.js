058091227971a5265090696df697d53d55c95f1c
NightYoshi370 <maorninja@outlook.com>
2019-10-17 21:54:31 -0700

    Fix invite again

--- commands/Bot Utilities/invite.js
+++ commands/Bot Utilities/invite.js
@@ -16,7 +16,7 @@ module.exports = class InviteCommand extends Command {
 
 		let embed = this.client.util.embed()
 			.setColor("RED")
-			.setDescription(global.lang.getString("If you need help, you can [join our support server]({0})!", this.client.supportServer))
+			.setDescription(global.lang.getString(msg.author.lang, "If you need help, you can [join our support server]({0})!", this.client.supportServer))
 
 		msg.channel.send("<:Yamamura:633898611125649418> | " + global.lang.getString(msg.author.lang, "Here's a link to invite Yamamura to your server: {0}", inviteLink), (this.client.supportServer && msg.channel.sendable ? {embed} : {}))
 
