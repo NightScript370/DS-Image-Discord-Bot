915b03f6d067306e342845ccf3417887aec4ddd8
NightYoshi370 <maorninja@outlook.com>
2019-11-24 10:33:55 -0800

    Move all import stuff to top of file

--- src/yamamura.js
+++ src/yamamura.js
@@ -1,40 +1,7 @@
-global.pathDirectory = __dirname;
-
-console.backlogs = {
-	debug: [],
-	errors: [],
-	others: []
-};
-
-// This is used to debug the errors.
-const util = require('util');
-const maxLog = 20;
-
-console.log = function () {
-	let logged = util.format.apply(null, arguments);
-
-	console.backlogs.others.push(logged);
-	process.stdout.write(logged + '\n');
-
-	if (console.backlogs.others.length > maxLog)
-		console.backlogs.others.shift();
-}
-console.error = function () {
-	let logged = util.format.apply(null, arguments);
-
-	console.backlogs.errors.push(logged);
-	process.stderr.write(logged + '\n');
-
-	if (console.backlogs.errors.length > maxLog)
-		console.backlogs.errors.shift();
-}
-
-
-try { import 'cache-require-paths' } catch {}
-import "./utils/extraFunctions.js";
+import 'cache-require-paths'
+import "./utils/extraFunctions";
 
 import * as translateModule from './langs/framework';
-global.translate = translateModule;
 
 import { AkairoClient, CommandHandler, InhibitorHandler, ListenerHandler } from 'discord-akairo';
 import { owners, supportServer, prefix as _prefix, website, log as _log, token } from "./config";
@@ -51,6 +18,36 @@ import "./struct/Guild";
 import "./struct/DMChannel";
 import "./struct/TextChannel";
 import "./struct/GuildMember";
+
+// This is used to debug the errors.
+import { format } from 'util';
+const maxLog = 20;
+
+global.translate = translateModule;
+console.backlogs = {
+	debug: [],
+	errors: [],
+	others: []
+};
+
+console.log = function () {
+	let logged = format.apply(null, arguments);
+
+	console.backlogs.others.push(logged);
+	process.stdout.write(logged + '\n');
+
+	if (console.backlogs.others.length > maxLog)
+		console.backlogs.others.shift();
+}
+console.error = function () {
+	let logged = format.apply(null, arguments);
+
+	console.backlogs.errors.push(logged);
+	process.stderr.write(logged + '\n');
+
+	if (console.backlogs.errors.length > maxLog)
+		console.backlogs.errors.shift();
+}
 
 class BotClient extends AkairoClient {
 	constructor() {
