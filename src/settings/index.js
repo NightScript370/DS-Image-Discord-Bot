27fbe569528a42faf0e78c90097ac288ea31b2f3
NightYoshi370 <maorninja@outlook.com>
2019-11-27 12:15:17 -0800

    Export getKey and findType functions

--- src/settings/index.js
+++ src/settings/index.js
@@ -7,7 +7,7 @@ let types = [];
 
 let propertyFileNames = readdirSync(join(__dirname, 'properties'))
 for (var propertyFileName of propertyFileNames) {
-	settingProps[propertyFileName.replaceAll('.js', '')] = require(join(__dirname, 'properties', propertyFileName))
+	settingProps[propertyFileName.split('.').slice(0, -1).join('.')] = import(join(__dirname, 'properties', propertyFileName))
 }
 
 let typesFileNames = readdirSync(join(__dirname, 'properties'))
@@ -15,11 +15,8 @@ for (var typeFileName of typesFileNames) {
 	types.push(require(join(__dirname, 'properties', typeFileName)));
 }
 
-function findType(key) {
-	return types.filter(type => type.id == settingProps[key].type)[0];
-}
-
-function getKey(client, msg, key) {
+export const findType = (key) => types.filter(type => type.id == settingProps[key].type)[0];
+export const getKey = (client, msg, key) => {
 	let data = databaseModule.serverconfig.findOne({guildID: msg.guild.id});
 
 	let value = data[key];
