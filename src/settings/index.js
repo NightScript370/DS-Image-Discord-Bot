7379974334d38f0dd025a003f82488b24c3cb5e1
NightYoshi370 <maorninja@outlook.com>
2019-11-27 15:47:31 -0800

    Remove __dirname

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,18 +1,18 @@
 import { readdirSync } from 'fs';
-import { join } from "path";
+import { join, resolve } from "path";
 import databaseModule from '../utils/database.js';
 
 let settingProps = {}
 let types = [];
 
-let propertyFileNames = readdirSync(join(__dirname, 'properties'))
+let propertyFileNames = readdirSync(join(resolve(), 'properties'))
 for (var propertyFileName of propertyFileNames) {
-	settingProps[propertyFileName.split('.').slice(0, -1).join('.')] = import(join(__dirname, 'properties', propertyFileName))
+	settingProps[propertyFileName.split('.').slice(0, -1).join('.')] = import(join(resolve(), 'properties', propertyFileName))
 }
 
-let typesFileNames = readdirSync(join(__dirname, 'properties'))
+let typesFileNames = readdirSync(join(resolve(), 'properties'))
 for (var typeFileName of typesFileNames) {
-	types.push(require(join(__dirname, 'properties', typeFileName)));
+	types.push(require(join(resolve(), 'properties', typeFileName)));
 }
 
 export const findType = (key) => types.filter(type => type.id == settingProps[key].type)[0];
